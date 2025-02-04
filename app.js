a49c1d27ad97aadf3124af9c9a755a28f5781689
rodrigo <rodrigo.gomes28599@al.aememmartins.pt>
2025-02-04 17:18:24 +0000

    final

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-const express=require('express');
-const mysql=require('mysql2');
-const axios=require("axios");
-const app=express()
-const port= 3000
+const express = require('express')
+const axios = require("axios");
+const app = express()
+const port = 3000
+ 
 app.use(express.json());
-app.set ('view engine',  'ejs');
-   
-app.get('/',(req,res)=>{
- let name =Roberto ;
- res.render('index',{nameOfTheClient:name  });
+ 
+
+app.set('view engine', 'ejs');
+ 
+app.get('/', (req,res) => {
+ 
+
+    res.render('main');
 })
+ 
+app.get('/lista', (req,res) => {
+ 
+    let name = "Roberto";
+    let listaCompras = ["carne", "sumo", "arroz"];
+    let msg = "A minha lista"
+ 
+    res.render('index', { nameOfTheClient: name, mensagem: msg, lista: listaCompras});
+})
+ 
+app.get('/contacts', (req,res) => {
+ 
+    res.render('contacts');
+})
+ 
+ 
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${3000}`)
+  })
